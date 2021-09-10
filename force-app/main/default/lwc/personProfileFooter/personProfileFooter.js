ac7f27ad39c28297c3c81c9e0e8a75c0eafe7002
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-09-10 22:30:48 +1000

    Add social media vector graphic image and update profile image

--- force-app/main/default/lwc/personProfileFooter/personProfileFooter.js
+++ force-app/main/default/lwc/personProfileFooter/personProfileFooter.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
+import PersonProfileResources from '@salesforce/resourceUrl/personProfileResources';
 
-export default class PersonProfileFooter extends LightningElement {}
+export default class PersonProfileFooter extends LightningElement {
+    facebookImg = PersonProfileResources + '/facebook.png';
+    linkedinImg = PersonProfileResources + '/linkedin.png';
+}
