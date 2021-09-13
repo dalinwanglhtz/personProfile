3e4bddf8cac31ea732d7978d731d35e857701b8d
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-09-13 23:22:47 +1000

    Load lenders log from static resources

--- force-app/main/default/lwc/personProfileServices/personProfileServices.js
+++ force-app/main/default/lwc/personProfileServices/personProfileServices.js
@@ -3,4 +3,14 @@ import PersonProfileResources from '@salesforce/resourceUrl/personProfileResourc
 
 export default class PersonProfileServices extends LightningElement {
     maggiePic = PersonProfileResources + '/maggiePic.jpg';
+    lenderPics = [
+        {
+            Id: 1,
+            src: PersonProfileResources + '/lenders/cba.png'
+        },
+        {
+            Id: 2,
+            src: PersonProfileResources + '/lenders/westpac.png'
+        }
+    ];
 }
