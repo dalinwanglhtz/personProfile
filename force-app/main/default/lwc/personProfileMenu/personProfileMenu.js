29573c32ffb3bfef76d293cbc88a96d5c3bc957a
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-09-23 23:05:12 +1000

    Refactor menu component

--- force-app/main/default/lwc/personProfileMenu/personProfileMenu.js
+++ force-app/main/default/lwc/personProfileMenu/personProfileMenu.js
@@ -8,6 +8,38 @@ import pageSwitch from '@salesforce/messageChannel/PageSwitchMessageChannel__c';
 export default class PersonProfileMenu extends LightningElement {
     @wire(MessageContext)
     messageContext;
+    menuItems = [
+        {
+            Id: 1,
+            menuClass: 'fix1',
+            menuName: 'home',
+            label: 'Home'
+        },
+        {
+            Id: 2,
+            menuClass: 'fix2',
+            menuName: 'aboutme',
+            label: 'About Me'
+        },
+        {
+            Id: 3,
+            menuClass: 'fix3',
+            menuName: 'services',
+            label: 'Services'
+        },
+        {
+            Id: 4,
+            menuClass: 'fix4',
+            menuName: 'testimony',
+            label: 'Testimony'
+        },
+        {
+            Id: 5,
+            menuClass: 'fix5',
+            menuName: 'contactme',
+            label: 'Contact Me'
+        }
+    ]
 
     handleClick(event) {
         const payload = {pageSelect: event.target.name};
